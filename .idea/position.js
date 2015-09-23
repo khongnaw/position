696ff662c7b676d298bb17796791b2d470a3dc1f
sora <android.prok@gmail.com>
2015-09-22 21:52:09 -1000

    Include professor Johnson's solution

--- .idea/position.js
+++ .idea/position.js
@@ -1,3 +1,4 @@
+
 /**
  * Created by sora1234 on 9/22/2015.
  */
@@ -11,12 +12,32 @@ function position(target,list){
       return i;
     }
   }
-
   return i
 
 }
 
+/*
 console.log(position(5,[1, 3, 5, 6]));
 console.log(position(2,[1, 3, 5, 6]));
 console.log(position(7,[1, 3, 5, 6]));
 console.log(position(0,[1, 3, 5, 6]));
+*/
+
+// Professor Johnson's solution
+
+function position2(num,nums){
+  var cursor = 0;
+  while((nums[cursor] <= num) && (cursor < nums.length))
+  {
+    if(nums[cursor] === num) {
+      return cursor;
+    }
+    cursor = cursor +1;
+  }
+  return cursor;
+}
+
+console.log(position2(5,[1, 3, 5, 6]));
+console.log(position2(2,[1, 3, 5, 6]));
+console.log(position2(7,[1, 3, 5, 6]));
+console.log(position2(0,[1, 3, 5, 6]));
